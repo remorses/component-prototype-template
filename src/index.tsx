7349067a4902186410c92cf4667e32caf131558a
Tommaso De Rossi <beats.by.morse@gmail.com>
2019-01-16 15:36:57 +0100

    playing around with codesandbox

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,13 @@ import ReactDOM from "react-dom";
 import styled from "styled-components";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello Ciao</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello Ciao</h1>
+            <h2>Start editing to see some magic happen!</h2>
+        </div>
+    );
 }
-
-const Container = styled.div;
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
